78fde1355454245a20c3137c488609692eaed070
premium_experte <levin.wiebelt@uni-goettingen.de>
2020-07-22 13:56:18 +0200

    updated simulation studies

--- simulation_studies/simulation_plot_4_params.R
+++ simulation_studies/simulation_plot_4_params.R
@@ -1,70 +1,52 @@
-# Since the alogrithm does not yet work properly for all possible models
-# this script analyses its functionality for 2-dim beta and gamma
-# also scale is regressed on the same predictor as location
-library(R6)
-library(asp20model)
 library(asp20boost)
 
-boost_true_model <- function(beta_0, beta_1, gamma_0, gamma_1){
-
-  # simulate data -------------------------
-  set.seed(1337)
-  n <- 500
-  x <- runif(n)
-  y <- beta_0 + beta_1 * x + rnorm(
-    n,
-    sd = exp(gamma_0 + gamma_1 * x)
-  )
-
-  # estimate data by boosting ------------
-  model <- LocationScaleRegressionBoost$new(y ~ x, ~ x)
-  gradient_boost(model, stepsize = 0.001, maxit = 5000, abstol = 0.0001, verbose = FALSE)
-
-
-  # plot results -------------------------
-  x_grid <- seq(0,1, length.out = 100)
-  y_grid <- beta_0 + beta_1 * x_grid
-
-  # limits for plot
-  ylim_upper <- model$beta[1] + model$beta[2] * 1 + 1.96 * exp(model$gamma[1] + model$gamma[2] * 1)
-  ylim_lower <- model$beta[1] + model$beta[2] * 1 - 1.96 * exp(model$gamma[1] + model$gamma[2] * 1)
-
-
-  # plot data points
-  plot(x, y, col = 2,
-       xlim = c(-0.3,1.3),
-       ylim = c(ylim_upper * 1.05, ylim_lower * 1.05))
-
-  # plot true model
-  lines(x_grid, y_grid)
-  lines(x_grid, y_grid + 1.96 * exp(gamma_0 + gamma_1 * x_grid))
-  lines(x_grid, y_grid - 1.96 * exp(gamma_0 + gamma_1 * x_grid))
-
-  #plot estimated effect
-  location_estimate <- model$beta[1] + model$beta[2] * x_grid
-  scale_estimate_sd <- exp(model$gamma[1] + model$gamma[2] * x_grid)
-  lines(x_grid, location_estimate, col = 3)
-  lines(x_grid, location_estimate + 1.96 * scale_estimate_sd, col = 3)
-  lines(x_grid, location_estimate - 1.96 * scale_estimate_sd, col = 3)
+# set true model parameters
+beta_0  <- 2
+beta_1  <- 2
+gamma_0 <- 2
+gamma_1 <- 2
 
 
 
-  return(list(
-    beta = c(model$beta[1], model$beta[2]),
-    gamma = c(model$gamma[1], model$gamma[2])
-  ))
-}
+# simulate data -------------------------
+set.seed(1337)
+n <- 500
+x <- runif(n)
+y <- beta_0 + beta_1 * x + rnorm(
+  n,
+  sd = exp(gamma_0 + gamma_1 * x)
+)
 
-#windows()
+# estimate data by boosting ------------
+model <- LocationScaleRegressionBoost$new(y ~ x, ~ x)
+gradient_boost(model, stepsize = 0.001, maxit = 5000, abstol = 0.00000000000000000000000000000000000000000001, verbose = TRUE)
 
-#boost_true_model(0,1,-3,2)
 
-#boost_true_model(0,0,0,0)
-#boost_true_model(0.5,0.5,0.5,0.5)
-#boost_true_model(1,1,1,1)
-#boost_true_model(2,2,2,2)
-#boost_true_model(-1,-1,-1,-1)
-#boost_true_model(-2,-2,-2,-2)
+# plot results -------------------------
+x_grid <- seq(0,1, length.out = 100)
+y_grid <- beta_0 + beta_1 * x_grid
 
-#boost_true_model(-2,-2,1,1)
-#boost_true_model(2,2,0,0.5)
+# limits for plot
+ylim_upper <- model$beta[1] + model$beta[2] * 1 + 1.96 * exp(model$gamma[1] + model$gamma[2] * 1)
+ylim_lower <- model$beta[1] + model$beta[2] * 1 - 1.96 * exp(model$gamma[1] + model$gamma[2] * 1)
+
+
+# plot data points
+plot(x, y, col = 2,
+     xlim = c(-0.3,1.3),
+     ylim = c(ylim_upper * 1.05, ylim_lower * 1.05))
+
+# plot true model
+lines(x_grid, y_grid)
+lines(x_grid, y_grid + 1.96 * exp(gamma_0 + gamma_1 * x_grid))
+lines(x_grid, y_grid - 1.96 * exp(gamma_0 + gamma_1 * x_grid))
+
+#plot estimated effect
+location_estimate <- model$beta[1] + model$beta[2] * x_grid
+scale_estimate_sd <- exp(model$gamma[1] + model$gamma[2] * x_grid)
+lines(x_grid, location_estimate, col = 3)
+lines(x_grid, location_estimate + 1.96 * scale_estimate_sd, col = 3)
+lines(x_grid, location_estimate - 1.96 * scale_estimate_sd, col = 3)
+
+model$beta
+model$gamma
