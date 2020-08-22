7dfefce0708ef52bf10d8d0bfd2169046a45555b
premium_experte <levin.wiebelt@uni-goettingen.de>
2020-08-22 19:31:40 +0200

    restructure model class and algorithm, implement helper functions for better overview

--- R/model.R
+++ R/model.R
@@ -28,235 +28,26 @@ LocationScaleRegressionBoost <- R6Class(
   "LocationScaleRegressionBoost",
   inherit = LocationScaleRegression,
 
-
-
-
-  private = list(
-
-    componentwise = TRUE,
-
-    X_HAT = numeric(),
-    X_HAT_componentwise = numeric(),
-    X_number_of_columns = numeric(),
-
-    Z_HAT = numeric(),
-    Z_HAT_componentwise = numeric(),
-    Z_number_of_columns = numeric(),
-
-    #Extend existing update beta to calculate least square estimates for current parameters
-    update_beta = function(value) {
-      super$update_beta(value)
-      #update least squares estimate only when model is initialized
-      if(length(private$X_number_of_columns)>0){
-        if(private$componentwise)
-        {
-          self$lstSqrEstBetaLossLoss_componentwise <- private$componentwiseLossBeta()
-        }
-        else
-        {
-
-          self$lstSqrEstBetaLoss <- private$X_HAT %*% crossprod(private$X, self$resid())
-        }
-      }
-    },
-
-    #Extend existing update gamma to calculate least square estimates for current parameters
-    update_gamma = function(value) {
-      super$update_gamma(value)
-
-      #update least squares estimate only when model is initialized
-      if(length(private$X_number_of_columns)>0){
-        if(private$componentwise){
-          self$lstSqrEstGammaLossLoss_componentwise <- private$componentwiseLossGamma()
-        }
-        else{
-          self$lstSqrEstGammaLoss <- private$Z_HAT %*% crossprod(private$Z, self$resid("deviance"))
-        }
-      }
-    },
-
-    #' @details
-    #' Returns the loss - squared error of the partial deviance for each covariate of Beta
-    #' @return
-    #' Vector of sum of squared errors for each covariate
-
-    componentwiseLossBeta = function(){
-
-      #Fit separate linear models for all covariates of beta
-      loss <- numeric()
-      resid_working <- (self$resid()) #  Our u_i for beta in page 226 step 2.
-
-      for(n in 1:private$X_number_of_columns) {
-        predictor_colwise <- private$X[,n]
-
-        # old way to do it: estimate residuals with predictor componentwise (columnwise)
-        # XX_inv_colwise <- t(predictor_colwise) %*% predictor_colwise)
-        # bjhat <- solve(XX_inv_colwise %*% t(predictor_colwise) %*% resid_working
-
-        # new way including cholesky calculations
-        bjhat <- private$X_HAT_componentwise[n] %*% crossprod(predictor_colwise, resid_working)
-
-        loss[n] <- sum((resid_working - predictor_colwise %*% bjhat) ^ 2)
-      }
-
-      return(loss)
-
-    },
-
-
-
-    #' @details
-    #' Returns the loss - squared error of the partial deviance for each covariate of Gamma
-    #' @return
-    #' Vector of sum of squared errors for each covariate
-
-    componentwiseLossGamma = function(){
-
-      #Fit separate linear models for all covariates of gamma
-      loss <- numeric()
-      resid_deviance <- self$resid("deviance") # Our u_i for gamma in page 226 step 2
-      resid_working <- self$resid()
-      number_of_columns <- dim(private$Z)[2]
-
-      for(n in 1:number_of_columns) {
-        predictor_colwise <- private$Z[,n]
-
-        # u -> first derivation of Log Likelihood - see documentation for further reading
-        grad_loss_function <- ((resid_working ^ 2) * predictor_colwise * exp(-2 * (drop(private$Z %*% self$gamma))) - predictor_colwise)
-        # for location-estimation the gradient of the loss function results in the residuals
-        # for scale-estimation another term results
-
-        #Kneib book Page 226
-
-        #Can be further improvied by moving ProjectionMatrix Calculation to initializalisation
-
-        # old way to calculate "residual-estimator" bjhat
-        # ZZ_inv_colwise <- solve(t(predictor_colwise) %*% predictor_colwise)
-        # bjhat <- ZZ_inv_colwise %*% t(predictor_colwise) %*% grad_loss_function
-
-        # new way including cholesky calculations
-        bjhat <- private$Z_HAT_componentwise[n] %*% crossprod(predictor_colwise, grad_loss_function)
-
-
-        # Calculate new squared error by subtracting the partial deviance of covariate with new gamma (first derivation u) of the total deviance
-        partial_deviance <- (resid_working / predictor_colwise %*% bjhat)
-        loss[n] <- sum((resid_deviance - partial_deviance) ^ 2) # Punkt 3 Formel
-      }
-
-      return(loss)
-    }
-
-  ),
-
   public = list(
-    lstSqrEstBetaLoss = numeric(),
-    lstSqrEstBetaLossLoss_componentwise = numeric(),
-    lstSqrEstGammaLoss = numeric(),
-    lstSqrEstGammaLossLoss_componentwise = numeric(),
 
-    par_log = list(),
-
-    initialize = function(location,
-                          scale = ~1,
-                          data = environment(location),
-                          ...) {
-      #first init super class LocationScaleRegression
-      super$initialize(location, scale,data)
-
-      private$X_number_of_columns <- dim(private$X)[2]
-      private$Z_number_of_columns <- dim(private$Z)[2]
-
-
-      private$X_HAT <- chol2inv(chol(crossprod(private$X, private$X)))
-      private$Z_HAT <- chol2inv(chol(crossprod(private$Z, private$Z)))
-
-
-      #init componentwise Hat Matrix to improve computation time for X
-      for(n in 1:private$X_number_of_columns) {
-        predictor_colwise <- private$X[,n]
-        private$X_HAT_componentwise[n] <- chol2inv(chol(crossprod(predictor_colwise, predictor_colwise)))
-      }
-      #init componentwise Hat Matrix to improve computation time for Z
-      for(n in 1:private$Z_number_of_columns) {
-        predictor_colwise <- private$Z[,n]
-        private$Z_HAT_componentwise[n] <- chol2inv(chol(crossprod(predictor_colwise, predictor_colwise)))
-      }
-
-      #Init least square estimates for components of beta and gamma
-      self$lstSqrEstBetaLossLoss_componentwise <- private$componentwiseLossBeta()
-      self$lstSqrEstGammaLossLoss_componentwise <- private$componentwiseLossGamma()
+    grad_loglik_mu = function() {
+      resid <- self$resid("response")
+      scale <- self$fitted_scale
+      return(resid / (scale^2) )
     },
 
-    #enable or disable componentwise boosting
-    setComponentwiseBoosting = function(value)
-    {
-      private$componentwise = value
+    grad_loglik_sigma = function() {
+      resid <- self$resid("response")
+      scale <- self$fitted_scale
+      return(resid^2 / scale^2 - 1)
     },
 
-
-
-    #' @details
-    #' Determines the best variable for componentwise boosting
-    #'
-    #' @return
-    #' New Beta Vector with updated values on the best fitted covariate
-    #' @export
-
-    coefficentUpdateBeta = function()
-    {
-      #build update vector
-      updateBeta <- replicate(length(model$beta), 0)
-      if(private$componentwise){
-      #determine index of the best-fitting variable
-      indexOfBetaUpdate = which.min(model$lstSqrEstBetaLossLoss_componentwise)
-
-
-
-      #Calculates current first derivates for Beta - the residuals
-      #updateBeta[indexOfBetaUpdate] <- solve(t(private$X[,indexOfBetaUpdate])%*%private$X[,indexOfBetaUpdate])%*% t(private$X[,indexOfBetaUpdate]) %*% model$resid()
-      updateBeta[indexOfBetaUpdate] <- private$X_HAT_componentwise[indexOfBetaUpdate] %*% crossprod(private$X[,indexOfBetaUpdate], model$resid())
-      }
-      else{
-
-        if(length(self$lstSqrEstBetaLoss)>0)
-        {
-          updateBeta <- updateBeta+self$lstSqrEstBetaLoss
-        }
-
-
-      }
-      updateBeta
-
+    X_pub = function() {
+      private$X
     },
 
-
-
-    #' @details
-    #' Determines the best variable for componentwise boosting
-    #' @return
-    #' New Gamma Vector with updated values on the best fitted covariate
-    #' @export
-
-    coefficentUpdateGamma = function()
-    {
-      #build update vector
-      updateGamma <- replicate(length(model$gamma), 0)
-      if(private$componentwise){
-      #determine index of the best-fitting variable, the covariate with the greates influance on the deviance will decrease the loss at most
-      indexOfGammaUpdate = which.min(model$lstSqrEstGammaLossLoss_componentwise)
-
-      #Calculates current first derivates for Gamma (index of the best-fitting variable) again like u, but as sum, since Gamma covariate is a single
-      updateGamma[indexOfGammaUpdate] <- sum((self$resid()^2)*private$Z[,indexOfGammaUpdate]*exp(-2*(drop(private$Z %*% self$gamma)))-private$Z[,indexOfGammaUpdate])
-      }
-      else{
-        if(length(self$lstSqrEstGammaLoss)>0)
-        {
-          updateGamma<- updateGamma+self$lstSqrEstGammaLoss
-        }
-      }
-
-
-      updateGamma
+    Z_pub = function() {
+      private$Z
     }
   )
 
@@ -285,111 +76,152 @@ LocationScaleRegressionBoost <- R6Class(
 #' gradient_boost(model)
 #'
 #' @export
+
 gradient_boost = function(model,
-                          stepsize = 0.001,
+                          stepsize = c(0.01, 0.1),
                           maxit = 1000,
-                          abstol = 0.001, componentwise = TRUE,
-                          verbose = TRUE, plot = TRUE) {
+                          abstol = 0.001,
+                          componentwise = TRUE,
+                          verbose = TRUE,
+                          plot = TRUE) {
 
-  model$setComponentwiseBoosting(componentwise)
-  grad_beta <- model$grad_beta()
-  grad_gamma <- model$grad_gamma()
-  v <- stepsize
-  #Page 226 init b0 wiht mean of y?
-  tmp_DerviatesB <- c(0,0)
-  tmp_DerviatesZ <- c(0,0)
+  # store gradients from last iteration step to compare them with newly calculated ones
+  grad_old_mu <- model$grad_loglik_mu()
+  grad_old_sigma <- model$grad_loglik_sigma()
 
+  for(iter in 1:maxit) {
 
-  model$par_log <- list()
+    if(componentwise == T) helper_boost_compwise(model, stepsize)
+    if(componentwise == F) helper_boost_conventional(model, stepsize)
 
-  #check if location scale model
-  for (i in seq_len(maxit)) {
+    # check for substantial change in gradients
+    grad_change_mu <- abs(grad_old_mu - model$grad_loglik_mu())
+    grad_change_sigma <- abs(grad_old_sigma - model$grad_loglik_sigma())
+    grad_changes <- c(grad_change_mu, grad_change_sigma)
 
-    #Set values for checking progress -> abstol
-    old_grad_beta <-grad_beta
-    old_grad_gamma <-grad_gamma
-
-    #needs custom step size for Beta, otherwise it would take to long ? increase with this factor
-    stepsizemultiplierbeta <-10
-
-    model$beta<-model$beta + stepsizemultiplierbeta*v*model$coefficentUpdateBeta()
-    model$gamma<-model$gamma + v*model$coefficentUpdateGamma()
-
-
-    grad_beta <- model$grad_beta()
-    grad_gamma <- model$grad_gamma()
-
-
-    model$par_log[[i]]<-c(model$beta, model$gamma)
-    if (verbose) {
-      par_msg <- c(model$beta, model$gamma)
-      par_msg <- format(par_msg, trim = TRUE, digits = 3)
-      par_msg <- paste(par_msg, collapse = " ")
-
-      grad_msg <- c(grad_beta, grad_gamma)
-      grad_msg <- format(grad_msg, trim = TRUE, digits = 3)
-      grad_msg <- paste(grad_msg, collapse = " ")
-
-      loglik_msg <- format(model$loglik(), digits = 3)
-
-      abs_msg <- format((abs(c(grad_beta, grad_gamma))), digits = 3)
-
-      message(
-        "Iteration:      ", i, "\n",
-        "Parameters:     ", par_msg, "\n",
-        "Gradient:       ", grad_msg, "\n",
-        "Squared Resid:       ", sum(model$resid()^2), "\n",
-        "Log-likelihood: ", loglik_msg, "\n",
-        "------------Beta---------------\n",
-        #"Beta Update Coeff: ", which.min(tmp_DerviatesB)," ", tmp_DerviatesB[indexOfBetaUpdate], "\n",
-        "------------Gamma---------------\n",
-        #"Gamma Update Coeff: ", which.min(tmp_DerviatesZ)," ", tmp_DerviatesZ[indexOfGammaUpdate], "\n",
-        #"ABS:",all(abs(c(grad_beta-old_grad_beta, grad_gamma-old_grad_gamma)))  , "\n",
-        "==============="
-      )
-
-
-    if (i>1&& all(abs(c(grad_beta-old_grad_beta, grad_gamma-old_grad_gamma)) <= abstol)) {
-      message("early stopping at iteration:",i)
-      #break()
+    if(iter > 1 && all(grad_changes) <= abstol){
+      message("early stopping at iteration: ",i)
+      break()
     }
 
+  #   if (verbose == T) {
+  #     par_msg <- c(model$beta, model$gamma)
+  #     par_msg <- format(par_msg, trim = TRUE, digits = 3)
+  #     par_msg <- paste(par_msg, collapse = " ")
+  #
+  #     grad_msg <- c(grad_beta, grad_gamma)
+  #     grad_msg <- format(grad_msg, trim = TRUE, digits = 3)
+  #     grad_msg <- paste(grad_msg, collapse = " ")
+  #
+  #     loglik_msg <- format(model$loglik(), digits = 3)
+  #
+  #     abs_msg <- format((abs(c(grad_beta, grad_gamma))), digits = 3)
+  #
+  #     message(
+  #       "Iteration:      ", i, "\n",
+  #       "Parameters:     ", par_msg, "\n",
+  #       "Gradient:       ", grad_msg, "\n",
+  #       "Squared Resid:       ", sum(model$resid()^2), "\n",
+  #       "Log-likelihood: ", loglik_msg, "\n",
+  #       "------------Beta---------------\n",
+  #       #"Beta Update Coeff: ", which.min(tmp_DerviatesB)," ", tmp_DerviatesB[indexOfBetaUpdate], "\n",
+  #       "------------Gamma---------------\n",
+  #       #"Gamma Update Coeff: ", which.min(tmp_DerviatesZ)," ", tmp_DerviatesZ[indexOfGammaUpdate], "\n",
+  #       #"ABS:",all(abs(c(grad_beta-old_grad_beta, grad_gamma-old_grad_gamma)))  , "\n",
+  #       "==============="
+  #     )
+  #
+  # }
+  #
+  # }
+  # if(plot == T) {
+  #   df <- data.frame(matrix(unlist(model$par_log), nrow=length(model$par_log), byrow=T))
+  #   plot(df$X1, type = "l",
+  #        ylim = c(min(df),max(df)),
+  #        xaxt = "n",
+  #        xlab = "No. of iterations",
+  #        ylab = "Parameter")
+  #   axis(1, at = c(1:dim(df)[1]), c(1:dim(df)[1]))
+  #   for(n in 2:dim(df)[2])
+  #   {
+  #     points(df[n], type = "l", col = "black")
+  #   }
+  # }
 
-  }
 
-  }
+  invisible(model)
 
-  if(plot)
-  {
-    df <- data.frame(matrix(unlist(model$par_log), nrow=length(model$par_log), byrow=T))
+}
 
-    plot(df$X1, type = "l",
-         ylim = c(min(df),max(df)),
-         xaxt = "n",
-         xlab = "No. of iterations",
-         ylab = "Parameter")
-    axis(1, at = c(1:dim(df)[1]), c(1:dim(df)[1]))
-    for(n in 2:dim(df)[2])
-    {
-      points(df[n], type = "l", col = "black")
-    }
-  }
+helper_boost_conventional <- function(model,
+                                      stepsize = c(0.01, 0.1)) {
+
+
+  X <- model$X_pub()
+  Z <- model$Z_pub()
+
+  number_of_cols_X <- dim(X)[2]
+  number_of_cols_Z <- dim(Z)[2]
+
+  gradients_mu <- model$grad_loglik_mu()
+  gradients_sigma <- model$grad_loglik_sigma()
+
+  gradient_estimators_mu <-  solve(t(X) %*% X) %*% t(X) %*% gradients_mu
+  gradient_estimators_sigma <-  solve(t(Z) %*% Z) %*% t(Z) %*% gradients_sigma
+
+  model$beta <- model$beta + stepsize[1] * gradient_estimators_mu
+  model$gamma <- model$gamma + stepsize[2] * gradient_estimators_sigma
 
   invisible(model)
 }
 
-# library(R6)
-# library(asp20model)
-# start_time <- Sys.time()
-# library(asp20model)
-# set.seed(1337)
-# n <- 500
-# x <- runif(n)
-# y <- x + rnorm(n, sd = exp(-3 + 2 * x))
-# model <- LocationScaleRegressionBoost$new(y ~ x, ~ x)
-# gradient_boost(model,stepsize = 0.001, maxit = 10000, abstol = 0.0001,componentwise = TRUE, verbose = TRUE, plot=TRUE)
-# end_time <- Sys.time()
-# end_time - start_time
 
 
+helper_boost_compwise <- function(model,
+                         stepsize = c(0.01, 0.1)
+                         ) {
 
+
+  X <- model$X_pub()
+  Z <- model$Z_pub()
+
+  number_of_cols_X <- dim(X)[2]
+  number_of_cols_Z <- dim(Z)[2]
+
+  gradients_mu <- model$grad_loglik_mu()
+  gradients_sigma <- model$grad_loglik_sigma()
+
+
+  gradient_estimators_mu <- rep(NA, number_of_cols_X)
+  gradient_estimators_sigma <- rep(NA, number_of_cols_Z)
+  losses_mu <- rep(NA, number_of_cols_X)
+  losses_sigma <- rep(NA, number_of_cols_Z)
+
+
+  for(j in 1:number_of_cols_X) {
+    gradient_estimators_mu[j] <-  solve(t(X[, j]) %*% X[, j]) %*% t(X[, j]) %*% gradients_mu
+    losses_mu[j] <- sum((gradients_mu - X[, j] * gradient_estimators_mu[j]) ^2)
+  }
+
+  for(j in 1:number_of_cols_Z) {
+    gradient_estimators_sigma[j] <-  solve(t(Z[, j]) %*% Z[, j]) %*% t(Z[, j]) %*% gradients_sigma
+    losses_sigma[j] <- sum((gradients_sigma - Z[, j] * gradient_estimators_sigma[j]) ^2)
+  }
+
+
+  least_loss_index_mu <- which.min(losses_mu)
+  least_loss_index_sigma <- which.min(losses_sigma)
+
+
+  update_vector_beta <- rep(0, number_of_cols_X)
+  update_vector_beta[least_loss_index_mu] <- gradient_estimators_mu[least_loss_index_mu]
+
+  update_vector_gamma <- rep(0, number_of_cols_X)
+  update_vector_gamma[least_loss_index_sigma] <- gradient_estimators_sigma[least_loss_index_sigma]
+
+
+  model$beta <- model$beta + stepsize[1] * update_vector_beta
+  model$gamma <- model$gamma + stepsize[2] * update_vector_gamma
+
+  invisible(model)
+}
