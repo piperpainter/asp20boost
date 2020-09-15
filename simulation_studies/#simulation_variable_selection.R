27b7093f5e7af40c7ffd2ce9bc70dfb6fafb3cec
levin <levin.wiebelt@uni-goettingen.de>
2020-09-15 08:12:53 +0200

    further developed sim studies

--- simulation_studies/#simulation_variable_selection.R
+++ simulation_studies/#simulation_variable_selection.R
@@ -1,12 +1,10 @@
 # This script analyses if variable selection in the case of multidemnsional
 # beta and gamma vectors works
 # For this reason some coefficients are 0.
-library(R6)
-library(asp20model)
 library(asp20boost)
 
-beta_vec <- c(1,0,1,0,1,0)
-gamma_vec <- c(0,1,0,1,0,1)
+beta_vec <- c(1,0,10,0,10,0)
+gamma_vec <- c(0,0.1,0,0.1,0,0.1)
 
 
 # beta_2 <- c(0,1,0,1,0,1)
@@ -41,9 +39,11 @@ y <- beta_vec[1] + beta_vec[2] * X[, 1] + beta_vec[3] * X[, 2] +  beta_vec[4] * 
 model <- LocationScaleRegressionBoost$new(
   y ~ X[, 1] + X[, 2] + X[, 3] + X[, 4] + X[, 5],
   ~ X[, 1] + X[, 2] + X[, 3] + X[, 4] + X[, 5])
-gradient_boost(model, stepsize = 0.001, maxit = 1000, abstol = 0.0001, verbose = FALSE)
+gradient_boost(model, stepsize = c(0.1, 0.01), maxit = 1000, verbose = TRUE, componentwise=T)
 
 
 model$beta
 model$gamma
 
+beta_vec
+gamma_vec
