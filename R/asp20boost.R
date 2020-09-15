6a631645807ea225449c4a038288a1eaffc0e5eb
Sebastian Aristizabal <j.aristizabalortiz@stud.uni-goettingen.de>
2020-09-15 12:25:18 +0200

    Hilfe Page

--- R/asp20boost.R
+++ R/asp20boost.R
@@ -1,21 +1,25 @@
-#' Gradient Boosting for Location-Scale Regression Models
+#' Gradient Boosting for Location-Scale Regression Models ANO ANO
 #'
 #' The package provides an R6-class `LocationScaleRegressionBoost` for model
-#' specification, and a function `gradient_boost` for boosting the specified model object.
+#' specification, and a function `gradient_boost` for boosting the specified
+#' model object.
 #'
 #' @usage
 #' model <- LocationScaleRegressionBoost(formula, ...)
 #' gradient_boost(model, ...)
 #'
 #' @details
-#' This package resulted from a series of student projects. Thus, it is part of a
-#' bundle of packages all starting with `asp20...`. The overall goal of these packages
-#' is to estimate location-scale regression models, while each project/package employs
-#' different methods. All packages, including `asp20boost` build on the central
-#' package `asp20model`. Here, an R6-class is specified, which is inherited and expanded
-#' in the `asp20boost`-package.
+#' This package resulted from a series of student projects in the context of the
+#' seminar *Advanced Statistical Programming 2020* of the University of
+#' Göttingen. Thus, it is part of a bundle of the `asp20__` packages. The main
+#' goal of these projects is implement a method to estimate location-scale
+#' regression models. Each project's package implements a different method. All
+#' packages, including `asp20boost`, build upon the `asp20model` package.
+#' Specifically, an R6-class with inherited properties from `asp20model` is
+#' `asp20model` expanded in the `asp20boost`-package.
 #'
-#' @author Johannes Strauß, Levin Wiebelt, Sebastiàn Aristizabal
+#'
+#' @author Johannes Strauß, Levin Wiebelt, Sebastián Aristizábal
 #'
 #' @import R6
 #' @import asp20model
